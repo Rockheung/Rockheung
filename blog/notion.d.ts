02c66209fac98e33cf26cc42283ce437642d8397
Rockheung <rockheung@gmail.com>
2022-07-01 13:29:56 +0900

    update: load notion pages

--- blog/notion.d.ts
+++ blog/notion.d.ts
@@ -3,9 +3,8 @@ import {
   RichTextItemResponse,
 } from "@notionhq/client/build/src/api-endpoints";
 
-type NotionType<T, K> = {
+type NotionType<T, K> = Record<T, K> & {
   type: T;
-  [T]: K;
 };
 
 type NotionTypeText = NotionType<
@@ -13,36 +12,31 @@ type NotionTypeText = NotionType<
   { content: string; link: string | null }
 >;
 
-type NotionProperty<T, K> = {
-  type: T;
-  [T]: K;
-  id: string;
-};
+type PageProperty<T, K> = NotionType<T, K> &
+  Record<T, K> & {
+    id: string;
+  };
 
-type NotionPropertyName = NotionProperty<
-  "title",
-  Array<RichTextItemResponse>
-> & {};
+type PagePropertyName = PageProperty<"title", Array<RichTextItemResponse>> & {};
 
-type NotionPropertyTags = NotionProperty<
+type PagePropertyTags = PageProperty<
   "multi_select",
   Array<SelectPropertyResponse>
 > & {};
 
-type NotionPropertyDate = NotionProperty<"date", DateResponse | null> & {};
-type NotionPropertyPublished = NotionProperty<"checkbox", boolean> & {};
-type NotionPropertyTldr = NotionPropertyNotionProperty<
+type PagePropertyDate = PageProperty<"date", DateResponse | null> & {};
+type PagePropertyPublished = PageProperty<"checkbox", boolean> & {};
+type PagePropertyTldr = PagePropertyPageProperty<
   "rich_text",
   Array<RichTextItemResponse>
 > & {};
 
 export type NotionPage = QueryDatabaseResponse["results"][number] & {
   properties: {
-    name: NotionPropertyName;
-    tags: NotionPropertyTags;
-    date: NotionPropertyDate;
-    published: NotionPropertyPublished;
-    tldr: NotionPropertyTldr;
-    1;
+    name: PagePropertyName;
+    tags: PagePropertyTags;
+    date: PagePropertyDate;
+    published: PagePropertyPublished;
+    tldr: PagePropertyTldr;
   };
 };
--- blog/pages/posts/[postId].tsx
+++ blog/pages/posts/[postId].tsx
@@ -1,24 +1,120 @@
-import { NextPage } from "next";
 import React from "react";
+import {
+  GetStaticProps,
+  InferGetServerSidePropsType,
+  NextPage,
+  GetStaticPropsResult,
+  GetStaticPaths,
+} from "next";
+import {
+  Client,
+  APIErrorCode,
+  isNotionClientError,
+  ClientErrorCode,
+} from "@notionhq/client";
 
-type PagePostProps = {
-  post: any;
+export const getStaticProps = async (context: { params: { postId: any } }) => {
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+
+  try {
+    if (
+      typeof context.params === "undefined" ||
+      typeof context.params.postId === "undefined" ||
+      Array.isArray(context.params.postId)
+    ) {
+      return {
+        props: {},
+        notFound: true,
+      };
+    }
+    const { postId } = context.params;
+    const page = await notion.pages.retrieve({ page_id: postId });
+    return {
+      props: {
+        post: page,
+      },
+    };
+  } catch (error: unknown) {
+    if (isNotionClientError(error)) {
+      // error is now strongly typed to NotionClientError
+      switch (error.code) {
+        case ClientErrorCode.RequestTimeout:
+          // ...
+          break;
+        case APIErrorCode.ObjectNotFound:
+          // ...
+          break;
+        case APIErrorCode.Unauthorized:
+          // ...
+          break;
+        // ...
+        default:
+          // you could even take advantage of exhaustiveness checking
+          console.log(error.code);
+      }
+    }
+  }
+
+  return {
+    props: {},
+  };
 };
 
-export async function getStaticProps() {
+export const getStaticPaths = async () => {
+  const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+  });
+
+  try {
+    const { results: pages } = await notion.databases.query({
+      database_id: process.env.NOTION_BLOG_DATABASE_ID || "",
+      filter: {
+        property: "published",
+        checkbox: {
+          equals: true,
+        },
+      },
+    });
+
+    return {
+      paths: pages.map((page) => {
+        return { params: { postId: page.id } };
+      }),
+      fallback: false,
+    };
+  } catch (error: unknown) {
+    if (isNotionClientError(error)) {
+      // error is now strongly typed to NotionClientError
+      switch (error.code) {
+        case ClientErrorCode.RequestTimeout:
+          // ...
+          break;
+        case APIErrorCode.ObjectNotFound:
+          // ...
+          break;
+        case APIErrorCode.Unauthorized:
+          // ...
+          break;
+        // ...
+        default:
+          // you could even take advantage of exhaustiveness checking
+          console.log(error.code);
+      }
+    }
+  }
+
   return {
-    props: {
-      post: {},
-    },
+    paths: [],
+    fallback: false,
   };
-}
+};
 
-export function getStaticPaths() {
-  return { paths: [], fallback: false };
-}
-
-const PagePost: NextPage<PagePostProps> = ({ post }) => {
-  return null;
+const PagePost: NextPage<
+  InferGetServerSidePropsType<typeof getStaticProps>
+> = ({ post }) => {
+  return <p>{JSON.stringify(post, null, 2)}</p>;
 };
 
 export default PagePost;
