7821cf5b7163cd80ef45b01f552a294e6437867a
Rockheung <rockheung@gmail.com>
2022-10-27 14:43:35 +0900

    update responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,23 +7,25 @@ import commonsConfig from "../commons.config";
 const Header = () => {
   return (
     <header className={styles.Header}>
-      <Link href={"/"}>
-        <a className={styles.Title}>
-          <Logo title={commonsConfig.blogName} />
-        </a>
-      </Link>
-      <nav className={styles.Nav}>
-        <Link href={"/posts"}>
-          <a className={styles.Link}>
-            <div>{"Posts"}</div>
+      <div className={styles.Nav_wrapper}>
+        <Link href={"/"}>
+          <a className={styles.Title}>
+            <Logo title={commonsConfig.blogName} />
           </a>
         </Link>
-        <Link href={"/about"}>
-          <a className={styles.Link}>
-            <div>{"About Me"}</div>
-          </a>
-        </Link>
-      </nav>
+        <nav className={styles.Nav}>
+          <Link href={"/posts"}>
+            <a className={styles.Link}>
+              <div>{"Posts"}</div>
+            </a>
+          </Link>
+          <Link href={"/about"}>
+            <a className={styles.Link}>
+              <div>{"About Me"}</div>
+            </a>
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
